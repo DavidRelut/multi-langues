105ff42bb403ed353c4bb45801a8526d802ffc32
David R <codingaccelerator@gmail.com>
2022-08-06 20:09:06 +0200

    creation of the logic to identify the language used by the browser

--- src/context/langContext.jsx
+++ src/context/langContext.jsx
@@ -2,6 +2,14 @@ import React, { createContext, useState } from 'react';
 
 export const Context = createContext();
 
+const supportedLangs = ['EN', 'FR', 'ES'];
+
+let browserLang = navigator.language.slice(0,2).toUpperCase();
+
+if(supportedLangs.indexOf(browserLang) === -1){
+   browserLang = 'EN';
+}
+
 const ContextProvider = props => {
   const [lang, setLang] = useState('EN');
 
